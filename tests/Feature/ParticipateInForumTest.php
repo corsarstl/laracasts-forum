0be66646bc5aee3106babdfea1eee1b6b349bf3b
Vitalii Syromiatnykov <corsarstl@gmail.com>
2018-02-28 21:28:03 +0200

    Lesson 4. 'A User May Respond to Threads' completed.

--- tests/Feature/ParticipateInForumTest.php
+++ tests/Feature/ParticipateInForumTest.php
@@ -7,9 +7,17 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 
-class ParticipateInForum extends TestCase
+class ParticipateInForumTest extends TestCase
 {
     use DatabaseMigrations;
+
+    /** @test */
+    public function unauthenticated_user_may_not_add_replies()
+    {
+        $this->expectException('Illuminate\Auth\AuthenticationException');
+
+        $this->post('threads/1/replies', []);
+    }
     
     /** @test */
     public function an_authenticated_user_may_participate_in_forum_threads()
@@ -18,9 +26,10 @@ class ParticipateInForum extends TestCase
 
         $thread = factory('App\Thread')->create();
 
-        $reply = factory('App\Reply')->create();
-        $this->post('/threads/'.$thread->id.'/replies', $reply->toArray());
+        $reply = factory('App\Reply')->make();
+        $this->post($thread->path().'/replies', $reply->toArray());
 
-        $this->get($thread->path());
+        $this->get($thread->path())
+            ->assertSee($reply->body);
     }
 }
