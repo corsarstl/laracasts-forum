0aa5aa1e61430d17ee88c4b2c2ba140e67022e70
Vitalii Syromiatnykov <corsarstl@gmail.com>
2018-03-17 21:06:31 +0200

    Lesson 11 'Users Can Filter Threads By Channel'

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Channel;
 use App\Thread;
 use Illuminate\Http\Request;
 
@@ -12,14 +13,20 @@ class ThreadsController extends Controller
         $this->middleware('auth')->except(['index', 'show']);
     }
 
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
+	/**
+	 * Display a listing of the resource.
+	 *
+	 * @param Channel $channel
+	 *
+	 * @return \Illuminate\Http\Response
+	 */
+    public function index(Channel $channel)
     {
-        $threads = Thread::latest()->get();
+    	if ($channel->exists) {
+    		$threads = $channel->threads()->latest()->get();
+	    } else {
+		    $threads = Thread::latest()->get();
+	    }
 
         return view('threads.index', compact('threads'));
     }
