67b479d6dffcc04e4a4d6af1a940a2fb5f25d73b
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-03-21 12:42:17 +0200

    Lesson 29. 'Flash Messaging With Vue'

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -14,6 +14,14 @@ try {
 } catch (e) {}
 
 /**
+ * Vue is a modern JavaScript library for building interactive web interfaces
+ * using reactive data binding and reusable components. Vue's API is clean
+ * and simple, leaving you to focus on building your next great project.
+ */
+
+window.Vue = require('vue');
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
@@ -51,3 +59,9 @@ if (token) {
 //     broadcaster: 'pusher',
 //     key: 'your-pusher-key'
 // });
+
+window.events = new Vue();
+
+window.flash = function (message) {
+    window.events.$emit('flash', message);
+};
