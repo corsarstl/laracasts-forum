32be42fc79f2a07a29cd66d6f91dd4b040191825
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-03-21 13:17:21 +0200

    Lesson 30. 'A User's Activity Feed Should Include Favorited Replies'

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,4 +1,4 @@
-<div class="panel panel-default">
+<div id="reply-{{ $reply->id }}" class="panel panel-default">
     <div class="panel-heading">
         <div class="level">
             <h5 class="flex">
@@ -18,9 +18,6 @@
                 </form>
             </div>
         </div>
-
-
-
     </div>
 
     <div class="panel-body">
