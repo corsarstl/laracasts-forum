0e15479fb69e64b2c1f34e3fcdc74841bee4ac42
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-03-23 16:30:20 +0200

    Lesson 44. 'Test Refactoring'

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -12,6 +12,9 @@
 */
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use Ramsey\Uuid\Uuid;
+
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     static $password;
 
@@ -54,5 +57,17 @@ $factory->define(App\Reply::class, function (Faker\Generator $faker) {
             return factory('App\User')->create()->id;
         },
         'body' => $faker->paragraph
+    ];
+});
+
+$factory->define(\Illuminate\Notifications\DatabaseNotification::class, function (Faker\Generator $faker) {
+    return [
+        'id' => Uuid::uuid4()->toString(),
+        'type' => 'App\Notifications\ThreadWasUpdated',
+        'notifiable_id' => function() {
+            return auth()->id() ?: factory('App\User')->create()->id;
+        },
+        'notifiable_type' => 'App\User',
+        'data' => ['foo' => 'bar']
     ];
 });
