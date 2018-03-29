3eef09b68569da57d4dde1747920948deda77d1c
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-03-29 15:36:02 +0300

    Lesson 73. 'Users Must Confirm Their Email Address: #3 - Cleanup'

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -2,10 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
-use App\User;
 use App\Http\Controllers\Controller;
-use Illuminate\Support\Facades\Validator;
+use App\Mail\PleaseConfirmYourEmail;
+use App\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
 
 class RegisterController extends Controller
 {
@@ -68,5 +71,10 @@ class RegisterController extends Controller
             'password' => bcrypt($data['password']),
             'confirmation_token' => str_random(25)
         ]);
+    }
+
+    protected function registered(Request $request, $user)
+    {
+        Mail::to($user)->send(new PleaseConfirmYourEmail($user));
     }
 }
