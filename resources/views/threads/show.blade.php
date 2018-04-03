ac95db3674721c70f6f4354e7aef173038852cc7
Syromiatnykov Vitalii <corsarstl@gmail.com>
2018-04-03 15:02:38 +0300

    Lesson 92. 'A Thread Can Be Updated: Part 2'

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -8,36 +8,8 @@
     <thread-view :thread="{{ $thread }}" inline-template>
         <div class="container">
             <div class="row">
-                <div class="col-8 col-sm-8">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <div class="level">
-
-                                <img src="{{ $thread->creator->avatar_path }}"
-                                     alt="{{ $thread->creator->name }}"
-                                     width="50" height="50"
-                                     class="mr-1">
-                                
-                                <span class="flex">
-                                    <a href="{{ route('profile', $thread->creator->name) }}">{{ $thread->creator->name }}</a>
-                                    posted: <b>{{ $thread->title }}</b>
-                                </span>
-
-                                @can('update', $thread)
-                                    <form action="{{ $thread->path() }}" method="POST">
-                                        {{ csrf_field() }}
-                                        {{ method_field('DELETE') }}
-
-                                        <button type="submit" class="btn btn-link">Delete Thread</button>
-                                    </form>
-                                @endcan
-                            </div>
-                        </div>
-
-                        <div class="panel-body">
-                            {{ $thread->body }}
-                        </div>
-                    </div>
+                <div class="col-8 col-sm-8" v-cloak>
+                    @include('threads.question')
 
                     <replies @added="repliesCount++" @removed="repliesCount--"></replies>
                 </div>
